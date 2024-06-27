9b79780a23349d2f62577fb9cfc1b6849b7c1dd5
Coffee (Le Truong Giang) <125234953+giangle-mfv@users.noreply.github.com>
2024-06-27 15:37:02 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -188,7 +188,7 @@ async function main() {
     readFileSync(process.env.GITHUB_EVENT_PATH ?? "", "utf8")
   );
 
-  if (eventData.action === "opened" || eventData.action === "pull_request") {
+  if (eventData.action === "opened" || eventData.action === "labeled") {
     diff = await getDiff(
       prDetails.owner,
       prDetails.repo,
