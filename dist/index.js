d8ef179421b129528ca0237d6a419203c4fe8663
Coffee <le.truong.giang@moneyforward.vn>
2024-07-05 14:54:09 +0700

    update prompt for pr description generation

--- dist/index.js
+++ dist/index.js
@@ -420,6 +420,23 @@ function createPRDescriptionPrompt(diff) {
     return `
   Your job is reading the following git diff and suggest me a description of the Pull Request. 
   The description should be summary of what i've done, what is the output and what can be the impact to the existing code base.
+  INSTRUCTIONS:
+    - Provide the response in markdown format
+    - Use the following template for the PR description:
+    1. **Why this PR is needed**
+      https://moneyforwardvietnam.atlassian.net/browse/SCI-
+    2. **What I did in this PR**
+
+    3. **How to check**
+
+    4. **Checklist**
+
+      - [] Pass all current \`unit-test\`
+      - [] I have performed a \`self-review\` and \`test\` of my code
+      - [] Check sql generated (if have) to ensure \`perf issue\`
+      - [] Do you have any \`new properties\`? If then you need to create properties
+      on: \`task_definition_app.json\`, \`task_definition_app_beta.json\` and \`AWS paramStore\`
+      - [] If this is PR to \`STG/PROD\` plz prepare \`liquibase script\`
   
   Git diff are:
   ---
